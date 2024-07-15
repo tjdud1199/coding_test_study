686cee0395ccbd1ca12db715aa1b78adfd77d6bf
tjdud1199 <69944948+tjdud1199@users.noreply.github.com>
2024-07-15 11:54:16 +0900

    [Bronze II] Title: Hashing, Time: 120 ms, Memory: 34760 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,20 @@
+import sys
+import string
+
+input = sys.stdin.readline
+
+alpha = string.ascii_lowercase
+
+L = int(input().rstrip())
+text = input().rstrip()
+r = 31
+M = 1234567891
+
+result = 0
+
+for i in range(L):
+    word = text[i]
+    a = alpha.find(word) + 1
+    result += a*r**i
+
+print(result)
