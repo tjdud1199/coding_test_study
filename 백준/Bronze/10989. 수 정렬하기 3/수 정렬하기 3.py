a40346e066094526a92f87451ef2f2635c028de6
tjdud1199 <69944948+tjdud1199@users.noreply.github.com>
2024-07-16 12:27:25 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9564 ms, Memory: 33188 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,17 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+nums = [0 for _ in range(10001)]
+
+for _ in range(n):
+    num = int(input().rstrip())
+    nums[num] += 1
+
+for i in range(1, 10001):
+    num = nums[i]
+    if num != 0:
+        for j in range(num):
+            print(i)
