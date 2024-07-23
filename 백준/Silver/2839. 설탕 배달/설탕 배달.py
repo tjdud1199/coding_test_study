f3f109c014cf093a4232d7cf1311ef7eb661f5cb
tjdud1199 <69944948+tjdud1199@users.noreply.github.com>
2024-07-23 22:30:15 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -3,22 +3,21 @@ import sys
 input = sys.stdin.readline
 
 n = int(input().rstrip())
-dp = [-1 for _ in range(n)]
+dp = [-1 for _ in range(n+1)]
 
-for i in range(3,n+1):
+dp[3] = 1
+
+if n >= 5:
+	dp[5] = 1
+
+for i in range(6,n+1):
     comp = []
-    idx = i-1
-    if i == 3:
-        dp[idx] = 1
-    elif i == 5:
-        dp[idx] = 1
-    else:
-        if dp[idx-3] != -1:
-            comp.append(dp[idx-3]+1)
-        if i >= 5:
-            if dp[idx-5] != -1:
-                comp.append(dp[idx-5]+1)
+    if dp[i-3] != -1:
+        comp.append(dp[i-3]+1)
+    if i >= 5:
+        if dp[i-5] != -1:
+            comp.append(dp[i-5]+1)
     if comp:
-        dp[idx] = min(comp)
+        dp[i] = min(comp)
 
-print(dp[n-1])
+print(dp[n])
