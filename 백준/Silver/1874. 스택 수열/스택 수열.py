0318733a264650c535e4fac78b235f1e1378838f
tjdud1199 <69944948+tjdud1199@users.noreply.github.com>
2024-07-29 17:08:31 +0900

    [Silver II] Title: 스택 수열, Time: 168 ms, Memory: 37396 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+now = 1
+sign = []
+stack = []
+temp = True
+
+for _ in range(int(input().rstrip())):
+    num = int(input().rstrip())
+    while now <= num:
+        stack.append(now)
+        sign.append('+')
+        now += 1
+    
+    check = stack.pop()
+    if check == num:
+        sign.append('-')
+    elif check > num:
+        temp = False
+    else:
+        stack.append(check)
+    
+if temp:
+    print(*sign, sep='\n')
+else:
+    print('NO')
