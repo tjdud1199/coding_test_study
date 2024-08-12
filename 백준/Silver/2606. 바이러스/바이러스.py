0d0746e7988688a5b456e99425c8c0b7068d58b5
tjdud1199 <69944948+tjdud1199@users.noreply.github.com>
2024-08-12 14:43:07 +0900

    [Silver III] Title: 바이러스, Time: 60 ms, Memory: 34068 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+n = int(input().rstrip())
+p = int(input().rstrip())
+
+graph = [[] for _ in range(n+1)]
+
+for _ in range(p):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+    
+visited = [False for _ in range(n+1)]
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
+
+bfs(graph, 1, visited)
+print(visited.count(True)-1)
